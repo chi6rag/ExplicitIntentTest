950e32958712b4bfb21a37c07ef208e0d34dad54
Chirag Aggarwal <chi6rag@gmail.com>
2015-06-23 12:17:07 +0530

    Add buttons to navigate across main->ActivityThree and back; main->ActivityTwo and back

--- app/src/main/java/net/chi6rag/android/explicitintenttest/ActivityTwo.java
+++ app/src/main/java/net/chi6rag/android/explicitintenttest/ActivityTwo.java
@@ -1,38 +1,31 @@
 package net.chi6rag.android.explicitintenttest;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityTwo extends ActionBarActivity {
 
+    Button button_go_back_to_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        button_go_back_to_main = (Button) findViewById(R.id.button_go_back_to_main);
+        button_go_back_to_main.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("chi6rag", "Clicked on button_go_back_to_main inside ActivityTwo");
+                        Intent intent = new Intent(ActivityTwo.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_b, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
